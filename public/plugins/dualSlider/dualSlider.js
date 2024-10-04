3945764e18688f0668335f2ee04f49fa17de5cf2
DESKTOP-7N6F09R\Sazgar <jaskndry4@gmail.com>
2024-10-04 01:30:33 -0700

    improve price slider (connect inputs and slider)

--- public/plugins/dualSlider/dualSlider.js
+++ public/plugins/dualSlider/dualSlider.js
@@ -1,22 +1,37 @@
-document.addEventListener("DOMContentLoaded", function () {
+function formatNumberWithCommas(value) {
+    return new Intl.NumberFormat('en-US').format(value);  // Format with commas
+}
+
+function removeCommas(value) {
+    return value.replace(/,/g, '');
+}
+
+function evaluateSlider(min, max, step){
     var rangeSliders = document.querySelectorAll(".price-slider");
     var minValueDisplays = document.querySelectorAll('.price-range-min');
     var maxValueDisplays = document.querySelectorAll('.price-range-max');
 
-    const maxValue = 100;
-    const minValue = 0;
-
     rangeSliders.forEach(slider =>{
         noUiSlider.create(slider, {
-            start: [20, 80],  // Initial values for the two handles
+            start: [min, max],  // Initial values for the two handles
             connect: true,    // Connect the handles with a colored bar
             range: {
-              'min': 0,
-              'max': 100
+              'min': min,
+              'max': max
             },
-            step: 1
+            step: step,
+            tooltips: [true, true],  // Enable tooltips for both handles
+            format: {
+                to: function(value) {
+                    return parseInt(value);  // Remove decimals
+                },
+                from: function(value) {
+                    return Number(value);
+                }
+            }
         });
 
+        //update slider base on inputs
         slider.noUiSlider.on('update', function (values) {
             minValueDisplays.forEach(display => {
                 display.value = values[0];
@@ -27,4 +42,33 @@ document.addEventListener("DOMContentLoaded", function () {
             })
         });
     })
+
+    //update inputs base on slider
+    minValueDisplays.forEach(input => {
+        input.addEventListener('change', function () {
+            rangeSliders.forEach(slider => {
+                slider.noUiSlider.set([this.value.replace(/\B(?=(\d{3})+(?!\d))/g, ','), null]);
+            })
+        })
+    })
+
+    maxValueDisplays.forEach(input => {
+        input.addEventListener('change', function () {
+            rangeSliders.forEach(slider => {
+                slider.noUiSlider.set([null, this.value.replace(/\B(?=(\d{3})+(?!\d))/g, ',')]);
+            })
+        })
+    })
+
+    minValueDisplays.forEach('input', function () {
+        this.value = this.value.replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+    })
+
+    maxValueDisplays.forEach('input', function () {
+        this.value = this.value.replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+    })
+}
+document.addEventListener("DOMContentLoaded", function () {
+    evaluateSlider(100, 1000000000,10000);
+    
 });
